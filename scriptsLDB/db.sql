89437e622ab25a2aea513523fc65515f804c8236
gabrielkyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-10-30 09:59:58 -0300

    Feito o diagrama de caso de uso, DER e o banco de dados.

---
+++ scriptsLDB/db.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE crud_kanban;
+USE crud_kanban;
+
+CREATE TABLE usuarios(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(120) NOT NULL,
+    email VARCHAR(255) NOT NULL
+    );
+    
+CREATE TABLE tarefas(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_usuario INT NOT NULL,
+    descricao VARCHAR(120) NOT NULL,
+    nome_setor VARCHAR(120) NOT NULL,
+    prioridade ENUM('baixa', 'media', 'alta') NOT NULL,
+    data_cadastro DATE NOT NULL,
+    status ENUM('a fazer', 'fazendo', 'pronto') NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+    );
